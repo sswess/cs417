a871dbbf7240a4190c3290de4f492264f7368162
monmon-2007 <minamonir11@hotmail.com>
2016-10-07 13:30:41 -0400

    First Commit

---
+++ AirportData/src/AirportData/Server.java
@@ -0,0 +1,25 @@
+package AirportData;
+
+//import AirportData.AirportDataProto.AirportList;
+import AirportData.AirportDataProto.AirportList;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class Server 
+{
+	public static void main(String[]args)
+	{
+		
+		
+		
+		
+		
+		
+	}
+
+}

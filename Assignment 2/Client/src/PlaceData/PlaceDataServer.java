61cc30bf0de71fb89808cd8c0052091672bf4512
sswess <kotenkanto@gmail.com>
2016-10-12 18:15:59 -0400

    put client code into assignment folder

---
+++ Assignment 2/Client/src/PlaceData/PlaceDataServer.java
@@ -0,0 +1,67 @@
+package PlaceData;
+import java.io.FileInputStream;
+
+import PlaceData.PlaceDataProto.Place;
+import PlaceData.PlaceDataProto.PlaceList;
+
+
+public class PlaceDataServer 
+{
+	public static void main(String[]args)
+	{
+		//Place.Builder place = new Place.newBuilder();
+		//PlaceList placeList;
+		try{	
+				PlaceList placeList = PlaceList.parseFrom(new FileInputStream("places-proto.bin"));
+				//sol(placeList);
+				
+				System.out.println(getInfo(placeList, "Hackberry town"));
+		}
+		catch(Exception e)
+		{
+			System.out.println("FILE NOT FOUND");
+		}
+		//System.out.println("Good to go BOI!!");
+		
+	}
+	
+		public static void sol(PlaceList placeList)
+		{
+			for (Place place: placeList.getPlaceList())
+			{	
+				System.out.println("Place: " + place.getName());
+			}
+		}
+	
+		public static String getInfo(PlaceList placeList, String city)
+		{
+			double lat,lon=0;
+			String state="";
+			String str="";
+			boolean flag = false;
+			for (Place place: placeList.getPlaceList())
+			{	
+				if(place.getName().equals(city))
+				{
+					flag = true;
+					lat = place.getLat();
+					lon = place.getLon();
+					state = place.getState();
+					str = place.getName()+","+state+","+Double.toString(lat)+","+Double.toString(lon);
+					break;
+				}
+				//System.out.println("Place: " + place.getName());
+			}
+			if(flag)
+			{
+				return str;
+			}
+			else
+				return "City Not Found";
+			
+			
+			//return "";
+		}
+	
+
+}
